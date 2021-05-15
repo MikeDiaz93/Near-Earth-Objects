a4674acc07069ad975bfa47638bf840972f1a3d4
MikelDietz <mikeoikos121@comunidad.unam.mx>
2021-05-15 15:45:27 -0500

    Update database.py
    Add classes and methods following the docstring conventions of PEP 257.

--- database.py
+++ database.py
@@ -3,32 +3,36 @@ from collections import defaultdict
 
 
 class NEODatabase:
-    """A database of near-Earth objects and their close approaches.
+    """
+    A class to represent a NeoDatabse object.
 
-    A `NEODatabase` contains a collection of NEOs and a collection of close
-    approaches. It additionally maintains a few auxiliary data structures to
-    help fetch NEOs by primary designation or by name and to help speed up
-    querying for close approaches that match criteria.
+    ...
+
+    Attributes
+    ----------
+    neos
+    approaches
+    collection_1
+    collection_2
+
+    Methods
+    -------
+    get_neo_by_designation(self, designation)
+    get_neo_by_name(self, name)
+    query(self, filters=())
+
     """
 
     def __init__(self, neos, approaches):
-        """Create a new `NEODatabase`.
+        """
+        Constructs all the necessary attributes for the NeoDatabase.
 
-        As a precondition, this constructor assumes that the collections of
-        NEOs and close approaches haven't yet been linked - that is, the
-        `.approaches` attribute of each `NearEarthObject` resolves to an empty
-        collection, and the `.neo` attribute of each `CloseApproach` is None.
-
-        However, each `CloseApproach` has an attribute (`._designation`) that
-        matches the `.designation` attribute of the corresponding NEO. This
-        constructor modifies the supplied NEOs and close approaches to link
-        them together - after it's done, the `.approaches` attribute of
-        each NEO has a collection of that NEO's close approaches, and the
-        `.neo` attribute of each close approach references the appropriate
-        NEO.
-
-        :param neos: A collection of `NearEarthObject`s.
-        :param approaches: A collection of `CloseApproach`es.
+        Parameters
+        ----------
+        neos: list
+        approches: list
+        collection_1: dict
+        collection_2: dict
         """
         self._neos = neos
         self._approaches = approaches
@@ -42,55 +46,51 @@ class NEODatabase:
                 neo.approaches.append(approach)
 
     def get_neo_by_designation(self, designation):
-        """Find and return an NEO by its primary designation.
+        """
+        Find and return an NEO by its primary designation.
 
-        If no match is found, return `None` instead.
-
-        Each NEO in the data set has a unique primary designation, as a string.
-
-        The matching is exact - check for spelling and capitalization if no
-        match is found.
-
-        :param designation: The primary designation of the NEO to search for.
-        :return: The `NearEarthObject` with the desired primary designation,
-        or `None`.
+        Parameters
+        ----------
+        designation: str
+                The primary designation of the NEO
+                to search for.
+        Return
+        ------
+        The NearEarthObject with the desired primary designation,
+        or None.
         """
 
         return self.collection_1.get(designation, None)
 
     def get_neo_by_name(self, name):
-        """Find and return an NEO by its name.
+        """
+        Find and return an NEO by its name.
 
-        If no match is found, return `None` instead.
+        Parameters
+        ----------
+        name: str
+            The name, as a string, of the NEO to search for.
 
-        Not every NEO in the data set has a name. No NEOs are associated with
-        the empty string nor with the `None` singleton.
-
-        The matching is exact - check for spelling and capitalization if no
-        match is found.
-
-        :param name: The name, as a string, of the NEO to search for.
-        :return: The `NearEarthObject` with the desired name, or `None`.
+        Return
+        ------
+        The NearEarthObject with the desired name, or None.
         """
 
         return self.collection_2.get(name, None)
 
     def query(self, filters=()):
-        """Query close approaches to generate those that match a collection
+        """
+        Query close approaches to generate those that match a collection
         of filters.
 
-        This generates a stream of `CloseApproach` objects that match all
-        of the provided filters.
-
-        If no arguments are provided, generate all known close approaches.
-
-        The `CloseApproach` objects are generated in internal order, which
-        isn't guaranteed to be sorted meaninfully, although is often sorted
-        by time.
-
-        :param filters: A collection of filters capturing user-specified
-        criteria.
-        :return: A stream of matching `CloseApproach` objects.
+        Parameters
+        ---------
+        filters: tuple
+                A collection of filters capturing user-specified
+                criteria.
+        Return
+        -------
+        A stream of matching CloseApproach objects.
         """
         for approach in self._approaches:
             if all(f(approach) for f in filters):

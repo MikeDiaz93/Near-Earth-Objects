803903552b648e08cd7d63a9f6543fbfc2f9fc7e
MikelDietz <mikeoikos121@comunidad.unam.mx>
2021-05-15 18:38:52 -0500

    Update write.py
    Update docstrings to all functions created.

--- write.py
+++ write.py
@@ -6,17 +6,15 @@ def write_to_csv(results, filename):
     """
     Write an iterable of CloseApproach objects to a CSV file.
 
-    Parameters
-    ----------
-    results: tuple
-        An iterable of CloseApproach objects.
-    filename: str
-        A Path-like object pointing to where the data
-        should be saved.
+            Parameters:
 
-    Return
-    ------
-    None
+                    results (tuple): An iterable of CloseApproach objects.
+                    filename (str): A Path-like object pointing to where
+                                the data should be saved.
+
+            Returns:
+
+                    None
     """
     fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s',
                   'designation', 'name', 'diameter_km',
@@ -33,17 +31,15 @@ def write_to_json(results, filename):
     """
     Write an iterable of CloseApproach objects to a JSON file.
 
-    Parameters
-    ----------
-    results: tuple
-        An iterable of CloseApproach objects.
-    filename: str
-        A Path-like object pointing to where the data
-        should be saved.
+            Parameters:
 
-    Return
-    ------
-    None
+                    results (tuple): An iterable of CloseApproach objects.
+                    filename (str): A Path-like object pointing to where
+                                the data should be saved.
+
+            Returns:
+
+                    None
     """
     approach_list = list()
     for approach in results:
